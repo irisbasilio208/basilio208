29ad31716c6a906a61796a36e929fb15c065bc5e
basilio208 <basilio208@hotmail.com>
2015-04-08 16:57:38 -0300

    Projeto Aula01 ver:1.1

--- Projeto Aula01/src/Ola.java
+++ Projeto Aula01/src/Ola.java
@@ -1,8 +1,13 @@
-
 public class Ola {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		for (int x = 1; x < 5; x++) {
+
+			System.out.println("Ola mundo!");
+
+		}
 
 	}
 
